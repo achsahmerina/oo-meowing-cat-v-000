d9e9439f35812ee9688704de03ea52cf9935e2f3
Achsah Abraham <achsahmp@gmail.com>
2018-08-10 18:49:58 +0000

    automatically backed up by learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,11 +1,9 @@
 ## code your solution here.
 class Cat
   attr_accessor :name
-  def cat_name
-    if :name == "maru"
-      return "meow!"
-    end
-    maru = Cat.name("Maru")
-    maru.name
-  end
+
 end
+
+maru = Cat.new
+maru.name = "meow"
+maru.name
